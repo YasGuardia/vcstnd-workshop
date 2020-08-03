268896bcea2cd763141269134e6c5276717b8cfd
Vladimir Castañeda <vladimir29castaneda@gmail.com>
2020-08-03 16:41:54 -0300

    first commit

---
+++ src/test/java/ar/com/ada/vcstnd/online/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.vcstnd.online.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
